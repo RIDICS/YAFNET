4133a984ac064af691312a01d08b403b4c7a6db1
Tomáš Hrabáček <hrabato2@fel.cvut.cz>
2019-08-13 15:16:41 +0200

    Set bigger size for columns Name and Description in table Forum

--- yafsrc/YetAnotherForum.NET/install/custom/vokabular-integration.sql
+++ yafsrc/YetAnotherForum.NET/install/custom/vokabular-integration.sql
@@ -1,15 +1,4 @@
-﻿/*
-** Put any custom sql you want run on reinstall in here.
-*/
-/*COMMIT
-
-ALTER DATABASE CURRENT
-SET READ_COMMITTED_SNAPSHOT ON WITH ROLLBACK IMMEDIATE
-
-SET XACT_ABORT ON
-BEGIN TRAN*/
-
--- Add column for mapping Category (Forum) - BookType (Vokabular)
+﻿-- Add column for mapping Category (Forum) - BookType (Vokabular)
 	ALTER TABLE [dbo].[yaf_Category] ADD ExternalID SMALLINT NULL UNIQUE
 
 -- Add column for mapping Forum (Forum) - Category (Vokabular)
@@ -18,4 +7,6 @@ BEGIN TRAN*/
 -- Add column for mapping Forum (Forum) - Project (Vokabular)
 	ALTER TABLE [dbo].[yaf_Forum] ADD ExternalProjectID BIGINT NULL
 
-/*COMMIT*/
+	ALTER TABLE [dbo].[yaf_Forum] ALTER COLUMN [Name] nvarchar(255) NOT NULL
+
+	ALTER TABLE [dbo].[yaf_Forum] ALTER COLUMN [Description] nvarchar(512) NULL
